5e5c52ef6003bc31cb6467a5c8427336c22076b1
Martin Koterle <martin.koterle@gmail.com>
2020-12-17 15:13:31 +0100

    Initial commit

---
+++ content/collections/pages/landing.md
@@ -0,0 +1,25 @@
+---
+title: Landing
+author: 46351542-da34-4d19-b34d-8fb7d7ee161e
+parent: 722793c8-92b9-40e5-894c-92e7e962d336
+seotamic_title: title
+seotamic_title_prepend: true
+seotamic_title_append: true
+seotamic_meta_description: empty
+seotamic_open_graph_title: title
+seotamic_open_graph_description: general
+seotamic_twitter_title: title
+seotamic_twitter_description: general
+updated_by: 46351542-da34-4d19-b34d-8fb7d7ee161e
+updated_at: 1608213743
+replicator:
+  -
+    type: hero_sections_-_simple_centered
+    enabled: true
+  -
+    type: hero_sections_-_split_with_navbar
+    enabled: true
+color: blue
+template: default
+id: 8378b553-dd8b-4e26-bb80-c2d0e07c4de6
+---
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,33 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-    purge: {
-      content: [
-        './resources/**/*.antlers.html',
-        './resources/**/*.blade.php',
-        './content/**/*.md'
-      ]
-    },
-    theme: {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+      colors: {
+        gray: colors.coolGray,
+        red: colors.red,
+        orange: colors. orange,
+        amber: colors.amber,
+        yellow: colors.yellow,
+        lime: colors.lime,
+        green: colors.green,
+        emerald: colors.emerald,
+        teal: colors.teal,
+        cyan: colors.cyan,
+        'light-blue': colors.lightBlue,
+        blue: colors.blue,
+        indigio: colors.indigo,
+        violet: colors.violet,
+        puprple: colors.purple,
+        fuschia: colors.fuschia,
+        pink: colors.pink,
+        rose: colors.rose
+      },
       extend: {},
-    },
-    variants: {},
-    plugins: [],
-  }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
